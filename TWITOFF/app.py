55328dacc7c19f126941408d0458c470c2e7d832
marilynle <marilynle@users.noreply.github.com>
2019-12-17 20:26:28 -0600

    files updated

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -10,7 +10,11 @@ def create_app():
     app = Flask(__name__)
 
     #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     #have the database know about the app
     DB.init_app(app)
 
@@ -18,4 +22,11 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title = 'Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+
     return app
